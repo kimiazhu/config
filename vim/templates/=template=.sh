1498a1840138aeb9237f5a6adced7ff1eb8e5f3f
ZHU HAIHUA <kimiazhu@gmail.com>
2018-11-21 11:29:36 +0800

    add custom sh template

---
+++ vim/templates/=template=.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Copyright %YEAR% %USER% <%MAIL%>.
+# All rights reserved.
+# Use of this source code is governed by MIT
+# license that can be found in the LICENSE file.
+#
+# Description: %FILE%
+# Author: %USER% <%MAIL%>
+# Since: %FDATE%
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+PLAIN='\033[0m'
+
+CWD=$(pwd)
