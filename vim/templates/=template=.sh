58ec425b6d470c4d0c469f2a10d2eafd3b825579
ZHU HAIHUA <kimiazhu@gmail.com>
2018-11-21 12:04:35 +0800

    feat: add common function

--- vim/templates/=template=.sh
+++ vim/templates/=template=.sh
@@ -14,3 +14,27 @@ YELLOW='\033[0;33m'
 PLAIN='\033[0m'
 
 CWD=$(pwd)
+
+function exits() {
+  if [[ -n "$2" ]]; then
+    echo $2
+  fi
+  cd $CWD
+  exit $1
+}
+
+# 获取参数1的绝对目录位置，请通过反引号获取返回值
+function absdir() {
+  if [[ -d "$1" ]]; then
+    cd $1
+    echo `pwd`
+	elif [[ -f "$1" ]]; then
+		cd `dirname $1`
+		echo `pwd`
+  else
+    exits 1 "no such file or directory: $1"
+  fi
+}
+
+#================== common function end ==================
+
